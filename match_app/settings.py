d55665d6f81dcd6a2c0a22e8e950219c8fe3ad14
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-06-01 06:21:33 +0100

    Update settings.py

--- match_app/settings.py
+++ match_app/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'logeddata@gmail.com'
-EMAIL_HOST_PASSWORD = 'logdatatxt'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'example'
 EMAIL_PORT = 587
 
 # Application definition
